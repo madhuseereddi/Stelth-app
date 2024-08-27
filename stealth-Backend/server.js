7ccd06cd6aa6668e0f6aa10eb076b85bf31ec566
madhuseereddi <seereddym@gmail.com>
2024-08-27 23:27:18 +0530

    api-stealth

---
+++ stealth-Backend/server.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const path = require("path");
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const regex = require("regex")
+
+const app = express();
+app.use(express.json());
+
+let db = null;
+
+
+let dbpath = path.join(__dirname, "db", "stealth.db");
+
+const DbServer = async () => {
+  try {
+    db = await open({
+      filename: dbpath,
+      driver: sqlite3.Database,
+    });
+
+    app.listen(3000, () => {
+      console.log("Server connected");
+    });
+  } catch (e) {
+    console.log(`Error occurred: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+DbServer();
+
+app.post("/login/", async (request, response) => {
+    let { email, password } = request.body;
+  
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{7,}$/;
+  
+    let userCheck = `SELECT * FROM users WHERE email = '${email}'`;
+  
+    try {
+      let result = await db.get(userCheck);
+  
+      if (result) {
+        const { password: storedPassword } = result;
+  
+        if (regex.test(password)) {
+          if (password === storedPassword){
+            payload = {email : email , userId : result.user_id}
+            let jwtToken = jwt.sign(payload , 'PASSWORD')
+            response.status(200)
+            response.send({jwtToken : jwtToken})
+         
+          }
+          else{
+            response.send({error : 'Password does not matched'})
+          }
+        } else {
+          response.send({ valid: false, message: 'Password does not meet the criteria' });
+        }
+      } else {
+        response.status(404).send({ message: 'User not found' });
+      }
+    } catch (error) {
+      response.status(500).send({ message: 'Internal server error' });
+    }
+});
+
+app.get("/locations/" , async (request , response) => {
+  let getQuery = `SELECT name,location , available_town from technicians`
+
+  let result = await db.all(getQuery)
+  response.send(result)
+
+})
+  
+app.get("/featured-technicians/" , async (request , response) => {
+  let getQuery = `SELECT name,photo,specialization,rating,description,repairing_items,available_town,location from technicians`
+
+  let result = await db.all(getQuery)
+  response.send(result)
+
+})
+
+app.get("/appliances/" , async (request , response) => {
+  let getQuery = `SELECT * from appliance_types`
+
+  let result = await db.all(getQuery)
+  response.send(result)
+
+})
+
+module.exports = app;
+
+
